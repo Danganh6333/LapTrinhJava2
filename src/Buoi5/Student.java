aff7f036ffa68494cca8eb19974c0c71b72639e1
Danganh6333 <danganh6785@gmail.com>
2023-11-10 13:15:19 +0700

    Init Project

---
+++ src/Buoi5/Student.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Buoi5;
+
+/**
+ *
+ * @author Admin
+ */
+public class Student {
+    private String Name;
+    private double Grade;
+
+    public Student(String Name, double Grade) {
+        this.Name = Name;
+        this.Grade = Grade;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public double getGrade() {
+        return Grade;
+    }
+
+    public void setGrade(double Grade) {
+        this.Grade = Grade;
+    }
+}
