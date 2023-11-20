940aa720fcfd3817bc540ff7d65d75f0c51228fc
Danganh6333 <danganh6785@gmail.com>
2023-11-20 13:00:23 +0700

    Init Project

--- src/Lab2/Bai1_1.java
+++ src/Lab2/Bai1_1.java
@@ -231,7 +231,7 @@ public class Bai1_1 extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
